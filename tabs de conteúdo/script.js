06b9103f33512f9b2f2325e597abfbf798b694ac
Hevelly <hevellycalabria@gmail.com>
2025-08-12 09:34:29 -0300

    Add files via upload
    tabs de conteudo

---
+++ tabs de conteúdo/script.js
@@ -0,0 +1,21 @@
+const tabs = document.querySelectorAll('.tab');
+const contents = document.querySelectorAll('.tab-content');
+const toggleThemeBtn = document.getElementById('toggleTheme');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const tabId = tab.dataset.tab;
+        tabs.forEach(t => t.classList.remove('active'));
+        tab.classList.add('active');
+        contents.forEach(content => {
+            content.classList.remove('active');
+            if (content.id === `tab-${tabId}`) {
+                content.classList.add('active');
+            }
+        });
+    });
+});
+toggleTheme.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    toggleTheme.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
